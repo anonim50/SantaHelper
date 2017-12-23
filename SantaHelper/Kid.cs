390c7ea3b5140400fe57b9ab3b83ad009012701e
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2017-12-23 23:28:39 +0200

    Add SantaGifts and Kid class with logic

---
+++ SantaHelper/Kid.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace SantaHelper
+{
+    public class Kid
+    {
+        const int MaxNumberOfGifts = 4;
+        private int _numberOfGiftsReceived;
+        public StringBuilder GiftsReceived { get; }
+
+        public Kid()
+        {
+            _numberOfGiftsReceived = 0;
+            GiftsReceived = new StringBuilder();
+        }
+
+        public void AddGift(string gift)
+        {
+            GiftsReceived.Append(GiftsReceived.Length == 0 ? gift : $" {gift}");
+            ++_numberOfGiftsReceived;
+        }
+
+        public bool NeedMoreGifts()
+        {
+            return _numberOfGiftsReceived < MaxNumberOfGifts;
+        }
+    }
+}
---
+++ SantaHelper/SantaGifts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace SantaHelper
+{
+    public class SantaGifts
+    {
+        public string[] Distribute(string[] gifts, int numberOfKids)
+        {
+            Kid[] kids = new Kid[numberOfKids];
+
+            for (var i = 0; i < numberOfKids; i++)
+            {
+                kids[i] = new Kid();
+            }
+
+            var currentKid = 0;
+
+            foreach (var gift in gifts)
+            {
+                if (kids[currentKid].NeedMoreGifts())
+                {
+                    kids[currentKid++].AddGift(gift);
+                    currentKid %= numberOfKids;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return GetGiftsFromKids(kids);
+        }
+
+        private string[] GetGiftsFromKids(Kid[] kids)
+        {
+            string[] gifts = new string[kids.Length];
+            for (var i = 0; i < kids.Length; i++)
+            {
+                gifts[i] = kids[i].GiftsReceived.ToString();
+            }
+            return gifts;
+        }
+    }
+}
